377abeb9a094e1db59c96a0da73b0a6b95eaa233
aion0721 <34002727+aion0721@users.noreply.github.com>
2022-10-19 23:47:50 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import "./styles.css";
+import { useTable, useSortBy } from "react-table";
+import { columns, data } from "./tableData";
+
+export default function App() {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable(
+    {
+      columns,
+      data
+    },
+    useSortBy
+  );
+  return (
+    <div className="App">
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  <span>
+                    {column.isSorted ? (column.isSortedDesc ? "a" : "b") : ""}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
---
+++ src/tableData.jsx
@@ -0,0 +1,24 @@
+export const columns = [
+  { Header: "level1", accessor: "level1" },
+  { Header: "level2", accessor: "level2" },
+  { Header: "level3", accessor: "level3" },
+  { Header: "level", accessor: "level" },
+  { Header: "content", accessor: "content" }
+];
+
+export const data = [
+  {
+    level1: "app",
+    level2: "react",
+    level3: "recoil",
+    level: 1,
+    content: "library"
+  },
+  {
+    level1: "app",
+    level2: "react",
+    level3: "test",
+    level: 2,
+    content: "library"
+  }
+];
